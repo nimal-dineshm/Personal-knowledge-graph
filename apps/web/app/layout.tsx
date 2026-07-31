4d6a022c7b5ea854efda4c6b9a2362c209b5df8f
Nimal Dinesh M <nimaldineshm128@gmail.com>
2026-07-31 23:47:26 +0530

    Add RootLayout component for the main application layout

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
+
+
+
+export const metadata: Metadata = {
+  title: "RAG Yt Chatapp",
+  description: "Chat with youtube lectures around AI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${GeistSans.className} h-full antialiased dark`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
